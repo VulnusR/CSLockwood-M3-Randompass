cbc4d64e7b4a6f40a4b85d37dac414a52ab5035c
Caleb Lockwood <caleblockwood@iMac.local>
2023-02-10 20:55:50 -0700

    Added a generatepassword function, needs some work still but I have included password length validation as apart of that function, and made minor JS syntax corrections.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,38 @@
 // Assignment code here
 //Characterset Constants for password generation
 
-const casesm = "abcdefghijklmnopqrstuvwxyz"
-const caselg = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const casesm = "abcdefghijklmnopqrstuvwxyz";
+const caselg = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const casenum = "1234567890"
-const casespc = "!@#$%^&*()_+-={}[]`~,.<>/?|"
+const casespc = "~!@#$%^&*()_+`{}|[]\,./<>?";
+
+
+//Generate Password Function
+function generatePassword() {
+
+//This creates the password length variable, and prompts the user to define the size of their desired password
+var passlength = prompt("Please enter your desired password length between 8 and 128 characters");
+
+//Password Length validation
+while (passlength < 8 | passlength > 128) {
+  passlength = prompt("Password length is outside of range parameters. 8-124 characters only.");
+};
+
+
+};
+
+
+
+
+
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+//Get reference to the #password elemet
+var passwordText =document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
